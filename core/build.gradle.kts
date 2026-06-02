1f7a4237e3bed81bf8cb79bd8a12c54d12312e90
Salman Bappi <salmanbappi@gmail.com>
2026-06-02 13:43:53 +0600

    fix: use modern kotlin block and jvmToolchain for AGP 9.0

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -27,12 +27,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-}
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
-        freeCompilerArgs.add("-opt-in=kotlinx.serialization.ExperimentalSerializationApi")
+    kotlin {
+        jvmToolchain(11)
+        compilerOptions {
+            freeCompilerArgs.add("-opt-in=kotlinx.serialization.ExperimentalSerializationApi")
+        }
     }
 }
 
