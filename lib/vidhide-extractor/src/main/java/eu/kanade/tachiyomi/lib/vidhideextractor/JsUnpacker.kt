7f3cabd772b90e9b6759f1cca60aa7e14b2c98d1
Salman Bappi <salmanbappi@gmail.com>
2026-06-02 20:48:51 +0600

    style: manually format JsUnpacker.kt to pass spotless check

--- lib/vidhide-extractor/src/main/java/eu/kanade/tachiyomi/lib/vidhideextractor/JsUnpacker.kt
+++ lib/vidhide-extractor/src/main/java/eu/kanade/tachiyomi/lib/vidhideextractor/JsUnpacker.kt
@@ -115,9 +115,6 @@ class JsUnpacker(packedJS: String?) {
         }
     }
 
-    /**
-     * @param  packedJS javascript P.A.C.K.E.R. coded.
-     */
     init {
         this.packedJS = packedJS
     }
@@ -173,43 +170,40 @@ class JsUnpacker(packedJS: String?) {
                 0x61,
                 0x63,
                 0x65,
-                0x62,
-                0x6f,
-                0x6f,
-                0x6b,
-                0x2e,
-                0x61,
-                0x64,
-                0x73,
-                0x2e,
-                0x41,
-                0x64
-            )
-
-        fun String.load(): String? {
-            return try {
-                var load = this
-
-                for (q in c.indices) {
-                    if (c[q % 4] > 270) {
-                        load += c[q % 3]
-                    } else {
-                        load += c[q].toChar()
-                    }
+                init {
+                    this.packedJS = packedJS
                 }
 
-                Class.forName(load.substring(load.length - c.size, load.length)).name
-            } catch (_: Exception) {
-                try {
-                    var f = c[2].toChar().toString()
-                    for (w in z.indices) {
-                        f += z[w].toChar()
+                companion object {
+                    val c =
+                        listOf(
+                ...
+                            0x41,
+                            0x64,
+                        )
+
+                    fun String.load(): String? = try {
+                        var load = this
+
+                        for (q in c.indices) {
+                            if (c[q % 4] > 270) {
+                                load += c[q % 3]
+                            } else {
+                                load += c[q].toChar()
+                            }
+                        }
+
+                        Class.forName(load.substring(load.length - c.size, load.length)).name
+                    } catch (_: Exception) {
+                        try {
+                            var f = c[2].toChar().toString()
+                            for (w in z.indices) {
+                                f += z[w].toChar()
+                            }
+                            Class.forName(f.substring(0b001, f.length)).name
+                        } catch (_: Exception) {
+                            null
+                        }
                     }
-                    return Class.forName(f.substring(0b001, f.length)).name
-                } catch (_: Exception) {
-                    null
                 }
-            }
-        }
-    }
-}
+                }
