140f8ff39a33b4ce7932eff852a41a274186aff4
salmanbappi <salmanbappi@users.noreply.github.com>
2026-06-20 02:29:52 +0600

    fix(en): Remove custom UA to fix Cloudflare bypass, and bump to v3

--- src/en/anidb/src/eu/kanade/tachiyomi/animeextension/en/anidb/AniDB.kt
+++ src/en/anidb/src/eu/kanade/tachiyomi/animeextension/en/anidb/AniDB.kt
@@ -59,7 +59,6 @@ class AniDB : Source() {
         .build()
 
     override fun headersBuilder() = super.headersBuilder()
-        .set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
         .set("Referer", "$baseUrl/")
 
     private val playlistUtils by lazy {
