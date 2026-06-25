709958731b01be16cfb3d2571ea6418966fd2855
salmanbappi <salmanbappi@users.noreply.github.com>
2026-06-25 12:32:54 +0600

    ReAnime: Remove custom User-Agent from headersBuilder

--- src/en/reanime/src/eu/kanade/tachiyomi/animeextension/en/reanime/ReAnime.kt
+++ src/en/reanime/src/eu/kanade/tachiyomi/animeextension/en/reanime/ReAnime.kt
@@ -99,7 +99,6 @@ class ReAnime : Source() {
 
 
     override fun headersBuilder() = super.headersBuilder()
-        .set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
         .set("Referer", "$baseUrl/")
 
     // ============================== Popular ===============================
