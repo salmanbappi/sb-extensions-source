d8c60ff1e0f77b3807f08f600413637d6520b50e
salmanbappi <salmanbappi@users.noreply.github.com>
2026-07-19 14:24:21 +0600

    Fix duplicate preferences override

--- src/en/animesaga/src/eu/kanade/tachiyomi/animeextension/en/animesaga/AnimeSaga.kt
+++ src/en/animesaga/src/eu/kanade/tachiyomi/animeextension/en/animesaga/AnimeSaga.kt
@@ -62,10 +62,6 @@ class AnimeSaga :
         }
     }
 
-    private val preferences: SharedPreferences by lazy {
-        Injekt.get<Application>().getSharedPreferences("source_$id", Application.MODE_PRIVATE)
-    }
-
     override fun headersBuilder() = super.headersBuilder()
         .add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
         .add("Referer", "$baseUrl/")
