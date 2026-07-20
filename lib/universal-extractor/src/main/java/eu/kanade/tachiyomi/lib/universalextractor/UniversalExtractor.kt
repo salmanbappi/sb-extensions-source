833ab374da256bc56ec97802a497eb2ce3e49eca
salmanbappi <salmanbappi@users.noreply.github.com>
2026-07-20 19:40:36 +0600

    Migrate Video constructor to v16 named arguments

--- lib/universal-extractor/src/main/java/eu/kanade/tachiyomi/lib/universalextractor/UniversalExtractor.kt
+++ lib/universal-extractor/src/main/java/eu/kanade/tachiyomi/lib/universalextractor/UniversalExtractor.kt
@@ -100,7 +100,11 @@ class UniversalExtractor(private val client: OkHttpClient) {
 
             "mp4" in resultUrl -> {
                 Log.d("UniversalExtractor", "mp4 URL: $resultUrl")
-                Video(resultUrl, "$prefix - $host: ${customQuality ?: "Mirror"}", resultUrl, origRequestHeader.newBuilder().add("referer", origRequestUrl).build()).let(::listOf)
+                Video(
+                    videoUrl = resultUrl,
+                    videoTitle = "$prefix - $host: ${customQuality ?: "Mirror"}",
+                    headers = origRequestHeader.newBuilder().add("referer", origRequestUrl).build()
+                ).let(::listOf)
             }
 
             else -> emptyList()
