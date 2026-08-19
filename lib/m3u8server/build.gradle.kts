662ba6bd89de6e8c731ca2aa094d98574832596c
salmanbappi <salmanbappi@users.noreply.github.com>
2026-08-19 11:42:04 +0600

    fix(deps): use com.github.NanoHttpd.nanohttpd:nanohttpd:master-SNAPSHOT

--- lib/m3u8server/build.gradle.kts
+++ lib/m3u8server/build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
 }
 
 dependencies {
-    implementation("com.github.NanoHttpd.nanohttpd:nanohttpd:nanohttpd-project-2.3.1")
+    implementation("com.github.NanoHttpd.nanohttpd:nanohttpd:master-SNAPSHOT")
 }
