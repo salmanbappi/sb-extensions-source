2fde0ee5cd4c90c2141a7c09e1ae24dfc38a9f69
salmanbappi <salmanbappi@users.noreply.github.com>
2026-08-23 16:06:40 +0600

    Add AniWave extension

---
+++ src/en/aniwave/src/eu/kanade/tachiyomi/animeextension/en/aniwave/AniWave.kt
@@ -0,0 +1,25 @@
+package eu.kanade.tachiyomi.animeextension.en.aniwave
+
+import eu.kanade.tachiyomi.animesource.model.AnimesPage
+import eu.kanade.tachiyomi.multisrc.anikototheme.AnikotoTheme
+import eu.kanade.tachiyomi.network.GET
+import extensions.utils.asJsoup
+
+class AniWave : AnikotoTheme() {
+
+    override val name = "AniWave"
+    override val baseUrl = "https://aniwaves.ru"
+    override val lang = "en"
+
+    override suspend fun getPopularAnime(page: Int): AnimesPage {
+        val url = if (page == 1) "$baseUrl/most-popular" else "$baseUrl/most-popular/page/$page"
+        val response = client.newCall(GET(url)).execute()
+        return parseAnimeList(response.asJsoup())
+    }
+
+    override suspend fun getLatestUpdates(page: Int): AnimesPage {
+        val url = if (page == 1) "$baseUrl/updated" else "$baseUrl/updated/page/$page"
+        val response = client.newCall(GET(url)).execute()
+        return parseAnimeList(response.asJsoup())
+    }
+}
