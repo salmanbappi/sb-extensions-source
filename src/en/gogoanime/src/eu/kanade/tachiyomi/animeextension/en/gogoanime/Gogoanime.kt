362d4567d67bbaf621eef8bf584c6d65588c3f97
salmanbappi <salmanbappi@users.noreply.github.com>
2026-09-06 17:22:53 +0600

    Remove stale m3u8server import breaking v5 build

--- src/en/gogoanime/src/eu/kanade/tachiyomi/animeextension/en/gogoanime/Gogoanime.kt
+++ src/en/gogoanime/src/eu/kanade/tachiyomi/animeextension/en/gogoanime/Gogoanime.kt
@@ -2,7 +2,6 @@ package eu.kanade.tachiyomi.animeextension.en.gogoanime
 
 import android.util.Base64
 import androidx.preference.PreferenceScreen
-import aniyomi.lib.m3u8server.M3u8Integration
 import eu.kanade.tachiyomi.animesource.model.AnimeFilter
 import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
 import eu.kanade.tachiyomi.animesource.model.AnimesPage
